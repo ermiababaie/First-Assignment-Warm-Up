0757d67decff0670a36f7d2f8fb4be6ae5e59f0c
ermia babaie <160699260+ermiababaie@users.noreply.github.com>
2024-02-29 16:39:33 +0330

    Update Exercises1.java

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -1,42 +1,33 @@
+import java.util.Scanner;
+
 public class Exercises1 {
-    /*
-    implement a function that returns factorial of given n
-     */
     public long factorial(int n) {
-        // TODO
-        return 0;
+        if (n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
     }
 
-
-    /*
-    implement a function that return nth number of fibonacci series
-    the series -> 1, 1, 2, 3, 5, 8, ...
-    */
-    public long fibonacci(int n) {
-        // TODO
-        return 0;
+    public long fibonacciSample(int n) {
+        if (n == 1 || n == 0)
+            return 1;
+        else
+            return (fibonacciSample(n - 1) + fibonacciSample (n - 2));
     }
 
-
-    /*
-    implement a function that returns a triangle for example:
-    row = 5
-
-    *
-    **
-    ***
-    ****
-    *****
-
-     */
-    public char[][] generateTriangle(int rows) {
-        // TODO
-        return null;
+    public char[][] generateTriangle(int row) {
+        char[][] c = new char[row][];
+        for (int i = 0; i < row; i++)
+            c[i] = new char[i + 1];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < i; j++) {
+                c[i][j] = '*';
+            }
+        }
+        return c;
     }
-
 
     public static void main(String[] args) {
-        // test your code here!
-    }
 
+    }
 }
